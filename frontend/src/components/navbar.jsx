19b20c2b4e45ef9902a2245a6184a607dcb1e508
mukulsingh24 <rmks1004@gmail.com>
2025-11-15 00:47:41 +0530

    some change s

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -5,36 +5,33 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { FaSearch } from "react-icons/fa";
 import { MdScanner } from "react-icons/md";
-import InputGroup from 'react-bootstrap/InputGroup';
+import InputGroup from "react-bootstrap/InputGroup";
 function Nav1() {
   return (
     <Navbar
       className="py-3"
-      style={{ backgroundColor:"#000053", color: "white" }}
+      style={{ backgroundColor: "#000053", color: "white" }}
     >
       <Navbar.Brand
         href="#home"
         className="fw-bold"
-        style={{ color: "white", marginLeft:'100px',fontSize:'30px'}}
+        style={{ color: "white", marginLeft: "100px", fontSize: "30px" }}
       >
         Task Mananger
       </Navbar.Brand>
-    
+
       <Nav style={{ marginLeft: 1300 }}>
-  <Button 
-    className="ps-4" 
-    style={{ padding: '8px 16px' }} // 8px top/bottom, 16px left/right
-  >
-    Login
-  </Button>
-  
-  <Button 
-    className="ps-4" 
-    style={{ padding: '8px 16px', marginLeft: '8px' }} // Added margin-left too
-  >
-    Signout
-  </Button>
-</Nav>
+        <Button className="ps-4" style={{ padding: "8px 16px" }}>
+          Login
+        </Button>
+
+        <Button
+          className="ps-4"
+          style={{ padding: "8px 16px", marginLeft: "8px" }}
+        >
+          Signout
+        </Button>
+      </Nav>
     </Navbar>
   );
 }
