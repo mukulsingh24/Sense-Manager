1f386ce5de973544f4feedd45a07d6aaa2c8d091
mukulsingh24 <rmks1004@gmail.com>
2025-11-16 14:46:54 +0530

    createted a login controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,18 +1,31 @@
 const User = require("../models/User");
-
-const UserController = async (req, res) => {
+const bcrypt = require('bcryptjs')
+const Register = async (req, res) => {
     // 200 == successfull
     if(await User.findOne({email: req.body.email})){
         res.status(200).json({message:"User Exists"})
     }
-    // 201 == new user // faulty in the existing one
     else{
         const newUser = new User({email:req.body.email,password:req.body.password})
         await newUser.save()
         res.status(200).json({message:"New user"})
 
-    }
-    
+    }    
 };
 
-module.exports = UserController;
+const Login = async(req,res) =>{
+    const user = await User.findOne({email:req.body.email})
+    if(user){
+        if(await bcrypt.compare(req.body.password,user.password)){
+        res.status(200).json({message:"Login Successfully"})
+    }
+    else{
+        res.status(200).json({message:"Invalid Credentails"})
+    }
+}
+    else{
+        res.status(200).json({message:"User Doesnt Exists"})
+    }
+}
+
+module.exports = {Register,Login};
