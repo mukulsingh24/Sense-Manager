8630c124d4ae6031ffc2ebbcc8869e06006eba4d
mukulsingh24 <rmks1004@gmail.com>
2025-11-17 13:47:32 +0530

    middleware completed

---
+++ backend/middleware/authmiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const Security = (req, res, next) => {
+  let token;
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith('Bearer')) {
+        try {
+      token = authHeader.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded; 
+      next();
+
+    } catch (error) {
+      res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  }
+  if (!token) {
+    res.status(401).json({ message: 'Not authorized, no token' });
+  }
+};
+module.exports = { Security };
